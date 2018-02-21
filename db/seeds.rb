7f95e275a2036f1d2b1607d3a8933c7975a38276
David Morley <davidkmorley@gmail.com>
2018-02-21 13:37:34 +0000

    created routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '01234 589 203',
+    category:     'japanese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '01234 589 203',
+    category:     'italian'
+  },
+  {
+    name:         'Pizza North',
+    address:      '56A Clapham High St, London E1 6PQ',
+    phone_number: '01234 589 203',
+    category:     'french'
+  },
+  {
+    name:         'Pizza West',
+    address:      '57 Shoreditch High St, London E1 6PQ',
+    phone_number: '01234 589 203',
+    category:     'belgian'
+  },
+  {
+    name:         'Pizza South',
+    address:      '59 Shoreditch High St, London E1 6PQ',
+    phone_number: '01234 589 203',
+    category:     'chinese'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
