1383f67f51d11b22d037646b4fd5d72b0903c2fb
David Morley <davidkmorley@gmail.com>
2018-02-21 15:32:30 +0000

    finished task with no styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,13 +8,23 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    # @review.save
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+
   end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
